5fc9d484977352664a1110b89eba6a3cf62c81b4
han <719400737@qq.com>
2020-03-08 22:33:57 +0800

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -2,7 +2,7 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-
+//遍历1遍，每次以遍历元素为中心，找两边元素是否相等，若不相等，则遍历后一个元素，若相等则继续遍历元素两边，获得最长的回文子串
 int main(){
     string s("cbaabaa");
     int len=s.size();
