24b17a840cbc7f4206f68e72e72afa16e255a1b4
719400737 <719400737@qq.com>
2020-04-25 20:10:20 +0800

    2020-04-25

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int a[5]={1,2,3,4,5};
-	for(auto i:a)
-		cout<<i<<" ";
-	cout<<"hello word"<<endl;
-	return 0;
-}
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-    int a;
-    cout<<1<<endl;
-    return 0;
-}
