ccb860d0365b4034a4a03737d7fd2f598e49c21f
719400737 <719400737@qq.com>
2020-06-02 21:47:28 +0800

    2020-06-02

---
+++ 57.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
